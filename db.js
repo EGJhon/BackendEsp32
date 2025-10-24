76fd14f7628ff69e977288b0dc62d69d94cc20a7
Thanatos <jeg.sistemas2001@gmail.com>
2025-10-24 09:30:11 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "admin", // 👈 tu usuario
-  host: "dpg-d384afggjchc73cotpa0-a", // 👈 solo el host, no la URL completa
-  database: "huerto", // 👈 tu base de datos
-  password: "mZKFQKsmoFidHwJpf6hKakSb4F6480IW",
+  user: "huertoesp_user", // tu usuario
+  host: "dpg-d3tom7mr433s73ds2thg-a", //solo el host, no la URL completa
+  database: "huertoesp", // tu base de datos
+  password: "9yVvR0VHaDh3EaAZ3PtmCuLoZd0f9mgA",
   port: 5432, // por defecto
 });
 
