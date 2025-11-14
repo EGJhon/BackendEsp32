3ab9654af8b66b3dc652d293e07e7e2149cbade4
Thanatos <jeg.sistemas2001@gmail.com>
2025-11-14 11:37:37 -0500

    hola

--- index.js
+++ index.js
@@ -408,6 +408,11 @@ userRouter.get("/sensores/ultimo", async (req, res) => {
     }
  });
 
+
+
+
+
+
 // Conectamos el router de usuario a la app
 app.use('/api', userRouter);
 
@@ -524,6 +529,50 @@ adminRouter.delete('/lecturas/:id', async (req, res) => {
 // Conectamos el router de admin a la app
 app.use('/api/admin', adminRouter);
 
+
+// Importar TF-Node y el modelo
+const tf = require('@tensorflow/tfjs-node');
+let iaModel = null;
+
+// Cargar el modelo UNA SOLA VEZ cuando el servidor inicia
+async function loadModel() {
+  try {
+    // ¡Carga el .h5 directamente!
+    const modelPath = 'file://./generic_model_9_features.h5'; 
+    iaModel = await tf.loadLayersModel(modelPath);
+    console.log('✅ Modelo de IA cargado en el backend');
+  } catch (err) {
+    console.error('❌ Error cargando modelo en backend:', err);
+  }
+}
+loadModel(); // Llama a la función al iniciar
+
+// ... en tu router (Express, etc.) ...
+app.post('/api/ia/predecir', async (req, res) => {
+  if (!iaModel) {
+    return res.status(500).send({ error: 'Modelo no está listo' });
+  }
+
+  // 1. Recibir las 9 variables crudas del app.js
+  const rawInput = req.body.input; // [temp, hum, sin, cos, ...]
+
+  // 2. Normalizar (¡necesitas las constantes MEAN y STD aquí!)
+ const IA_MEAN = [19.217487787857642, 71.75930914166085, 0.0016620385105260921, 0.067683629093006, 15.149685973482205, 25.059316120027912, 69.52023726448012, 79.52023726448012,22.835387299371945];
+const IA_STD = [2.4249451811563576, 12.748688156611234, 0.6839458433540213, 0.7266234840248392, 0.733345561704264, 0.4185557565303383, 2.2740148501713517, 2.2740148501713517,0.8606089852621961];
+
+  const normalizedInput = rawInput.map((val, i) => (val - IA_MEAN[i]) / IA_STD[i]);
+
+  // 3. Predecir
+  const inputTensor = tf.tensor2d([normalizedInput]);
+  const prediction = iaModel.predict(inputTensor);
+  const humedadFutura = (await prediction.data())[0];
+
+  inputTensor.dispose();
+  prediction.dispose();
+
+  // 4. Devolver solo el resultado
+  res.json({ prediccion: humedadFutura });
+});
 
 // --------------------
 // Iniciar servidor
